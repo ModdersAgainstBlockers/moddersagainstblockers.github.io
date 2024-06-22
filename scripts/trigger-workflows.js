fa8e416667b45299027dc242ec819f6df453ae9d
fxmorin <28154542+fxmorin@users.noreply.github.com>
2024-06-22 17:41:36 -0400

    Octokit doesn't like running within github actions - https://github.com/octokit/octokit.js/issues/2530

---
+++ scripts/trigger-workflows.js
@@ -0,0 +1,88 @@
+const { request } = require("@octokit/request");
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const { readFileSync } = require('fs');
+
+// Read environment variables
+const appId = process.env.APP_ID;
+const privateKey = readFileSync('private_key.pem', 'utf-8');
+const workflowIds = JSON.parse(fs.readFileSync('workflow_ids.json', 'utf-8'));
+
+async function generateJWT(appId, privateKey) {
+    const payload = {
+        iat: Math.floor(Date.now() / 1000),
+        exp: Math.floor(Date.now() / 1000) + (10 * 60), // 10 minutes
+        iss: appId
+    };
+    return jwt.sign(payload, privateKey, { algorithm: 'RS256' });
+}
+
+async function getInstallations(jwt) {
+    const response = await request("GET /app/installations", {
+        headers: {
+            authorization: `Bearer ${jwt}`,
+            accept: "application/vnd.github.v3+json"
+        }
+    });
+    return response.data;
+}
+
+async function getAccessToken(jwt, installationId) {
+    const response = await request("POST /app/installations/{installation_id}/access_tokens", {
+        headers: {
+            authorization: `Bearer ${jwt}`,
+            accept: "application/vnd.github.v3+json"
+        },
+        installation_id: installationId
+    });
+    return response.data.token;
+}
+
+async function getRepositories(accessToken) {
+    const response = await request("GET /installation/repositories", {
+        headers: {
+            authorization: `token ${accessToken}`,
+            accept: "application/vnd.github.v3+json"
+        }
+    });
+    return response.data.repositories;
+}
+
+async function triggerWorkflow(repo, accessToken, data) {
+    await request("POST /repos/{owner}/{repo}/dispatches", {
+        headers: {
+            authorization: `token ${accessToken}`,
+            accept: "application/vnd.github.v3+json"
+        },
+        owner: repo.split('/')[0],
+        repo: repo.split('/')[1],
+        event_type: "custom_event",
+        client_payload: data
+    });
+}
+
+async function main() {
+    const jwt = await generateJWT(appId, privateKey);
+    const installations = await getInstallations(jwt);
+
+    for (const installation of installations) {
+        const accessToken = await getAccessToken(jwt, installation.id);
+        const repositories = await getRepositories(accessToken);
+
+        for (const repo of repositories) {
+            const repoName = repo.full_name.split('/')[1];
+            const customData = workflowIds[repoName];
+            if (customData) {
+                console.log(`Triggering workflow for ${repo.full_name}`);
+                await triggerWorkflow(repo.full_name, accessToken, customData);
+            } else {
+                console.log(`No data found for ${repoName} in workflow_ids.json`);
+            }
+        }
+    }
+}
+
+main().catch(err => {
+    console.error(err);
+    process.exit(1);
+});
